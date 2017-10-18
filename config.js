3f6d7435e0c99a193907d2b72fb0e1f787cd7ae3
Kreed <derekandrews@protonmail.com>
2017-10-18 23:53:19 +0000

    committing the code

--- config.js
+++ config.js
@@ -1,15 +1,8 @@
 // config.js
 
 module.exports = {
-  consumer_key: '24g5xvSBoY4i6eQLIBvNjmV2i',
-  consumer_secret: 'xGeY02TlGDzMZqrgzG0FxsLdcnaC4MxDZUU3hl9kBallNF0FQG',
-  access_token: '157630046-22GONAFe6nm7JTDw4pmp9JWJaoLWCEDe7oGFRmOo',
-  access_token_secret: 'qvQyzbR4AdyJeuZFU6uv9s8AkbonhEDUaWyBgfhX6ovuw'
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 };
-
-// module.exports = {
-//   consumer_key: process.env.BOT_CONSUMER_KEY,
-//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
-//   access_token: process.env.BOT_ACCESS_TOKEN,
-//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-// };
